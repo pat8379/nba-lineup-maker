cb750a3d0c53f272fb60ebceb384aee5c923b1ad
Roger Deakins <patrickw7405@gmail.com>
2022-06-07 09:03:50 +0700

    edit on package.json

--- server.js
+++ server.js
@@ -2,14 +2,20 @@
 // console.log(data)
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const PORT = process.env.PORT || 4000;
 
 // Add middleware for handling CORS requests from index.html
 const cors = require('cors');
 app.use(cors());
+// app.use(express.static('public'));
+
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static('public'));
+  app.use(express.static('build'));
+  app.get("*",(req,res) => {
+    req.sendFile(path.resolve(__dirname,'build','index.html'))
+  })
 }
 
 // Add middware for parsing request bodies here:
